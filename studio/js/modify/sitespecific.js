72e00b5fcc81c5ec87cd1c4095d6f4b7f2a079e0
Michael Kelly <email@michaelevan.com>
2019-01-07 16:08:25 -0800

    Started added some basic arrow vis to the project

--- studio/js/modify/sitespecific.js
+++ studio/js/modify/sitespecific.js
@@ -2,3 +2,52 @@ function pageBuilder(){
 	var $papa = $j(this);
 
 }
+
+function buildVis() {
+	// create an array with nodes
+	var nodes = new vis.DataSet([
+		{id: 1, label: 'A'},
+		{id: 2, label: 'B'},
+		{id: 3, label: 'C'},
+		{id: 4, label: 'D'}
+	]);
+
+	// create an array with edges
+	var edges = new vis.DataSet([
+		{from: 1, to: 2, arrows:'to'},
+		{from: 2, to: 3, arrows:{
+				to: {
+					enabled: true,
+					type: 'circle'
+				}
+			}},
+		{from: 3, to: 4, arrows:{
+				to: {
+					enabled: true,
+					type: 'bar'
+				}
+			}},
+	]);
+
+	// create a network
+	var container = document.getElementById('mynetwork');
+	var data = {
+		nodes: nodes,
+		edges: edges
+	};
+
+	var options = {
+		/*
+        // Enable this to make the endpoints smaller/larger
+        edges: {
+          arrows: {
+            to: {
+              scaleFactor: 5
+            }
+          }
+        }
+    */
+	};
+
+	var network = new vis.Network(container, data, options);
+}
