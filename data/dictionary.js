c04ec09031692b2197d2094730d90878ff007a10
Michael Kelly <email@michaelevan.com>
2019-01-08 18:42:54 -0800

    Simplified the dictionary object which i’m starting to reallky call the entity object

--- data/dictionary.js
+++ data/dictionary.js
@@ -1,35 +1,27 @@
 $j.what("dictionary", {
   application: {
-    pas: {
-      id:1,
+    1: {
       label:"PAS"
     },
-    mdm: {
-      id:2,
+    2: {
       label:"MDM"
     },
-    soa: {
-      id:3,
+    3: {
       label:"SOA"
     },
-    digicom: {
-      id:4,
+    4: {
       label:"Digital Communications"
     },
-    dcs: {
-      id:5,
+    5: {
       label:"DCS"
     },
-    deliveryEngine: {
-      id:6,
+    6: {
       label:"Delivery Engine"
     },
-    evtConsumer: {
-      id:7,
+    7: {
       label:"Event's Consumer"
     },
-    customer: {
-      id:8,
+    8: {
       label:"Customer"
     }
   }
