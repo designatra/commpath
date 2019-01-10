8d7fa78abe2a8e091249ed3f69ab9886f40ba81e
Michael Kelly <email@michaelevan.com>
2019-01-10 15:25:05 -0800

    housekeeping

--- studio/js/modify/sitespecific.js
+++ studio/js/modify/sitespecific.js
@@ -139,15 +139,6 @@ function buildVis() {
     type:"network"
   });
 }
-function guidGenerator() {
-  var S4 = function() {
-    return(((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  };
-  return(S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-}
-var guid = function() {
-  return guidGenerator();
-}
 
 
 
