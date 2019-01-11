f6215986831beeba25ab5f299227bacbe4820eca
Michael Kelly <email@michaelevan.com>
2019-01-11 07:33:33 -0800

    Added first two dice formats and convience method
    $j.dice("role", "eventInterval", "sides6")

--- studio/js/modify/dice.js
+++ studio/js/modify/dice.js
@@ -4,7 +4,22 @@
 		methods: {},
 		init: false,
 		dice:{},
-		data: {}
+		formats: {
+			sides6:"d6",// or 1d6 (A 6 sided die)
+			sides6x2:"2d6"// (Two 6 sided dice)
+			// 1d6+4 (Roll a 6 sided dice and add 4 to the result)
+			// 2d10*4+1d20 (Roll two 10 sided dice multiply by four, and roll one 20 sided die)
+			// 2d10+4+2d20-L (Roll two 10 sided dice add four, and roll two 20 sided die, taking away the lowest of the two)
+			// d% (A percentile die - equivalent to d100)
+			// dF or dF.2 (A standard fudge dice - 2 thirds of each symbol)
+			// dF.1 (A non-standard fudge dice - 1 positive, 1 negative, 4 blank)
+			// 2d6! (Exploding dice - two 6 sided die, rolling again for each roll of the maximum value)
+			// 2d6!! (Exploding & compounding dice - like exploding, but adding together into single roll)
+			// 2d6!p (Penetrating dice - like exploding, but subtract 1 from each consecutive roll)
+			// 2d6!!p (Penetrating & compounding dice - like exploding & compounding, but subtract 1 from each consecutive roll)
+			// 2d6!>=4 (Exploding dice, but only if you roll a 4 or greater - Also usable with compounding and penetrating dice)
+			// 2d6>4 (Dice pool - anything greater t
+		}
 	};
 
 	plugin.methods.dom = {
@@ -43,12 +58,18 @@
 		core: function() {
 			return $j.what();
 		},
-		role: function(id) {
+		/*
+				$j.dice("role", "eventInterval", "sides6")
+		*/
+		role: function(id, format) {
 				var die = plugin.dice[id];
 				if(!die) {
-					die = plugin.dive[id] = new DiceRoller();
+					die = plugin.dice[id] = new DiceRoller();
 				}
-				
+				die.roll(plugin.formats[format]);
+				return die.log.shift().total;
+		},
+		formats: function() {
 
 		}
 	};
